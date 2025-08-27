8289ffec0f18c61ce860bb170e99675a1b693c80
Carlos Precioso <carlos@wasp-lang.dev>
2025-08-27 14:46:02 +0200

    Add check/uncheck

---
+++ app/Livewire/Tasks/Item.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Livewire\Tasks;
+
+use App\Models\Task;
+use Livewire\Component;
+
+class Item extends Component
+{
+
+    public Task $task;
+
+    public function render()
+    {
+        return view("livewire.tasks.item");
+    }
+
+    public function handleCheck()
+    {
+        $this->task->is_completed = true;
+        $this->task->save();
+    }
+
+    public function handleUncheck()
+    {
+        $this->task->is_completed = false;
+        $this->task->save();
+    }
+}
