a4869ef310dda297fe2df61b3cd6cd4ea9fc9c6a
Carlos Precioso <carlos@wasp-lang.dev>
2025-08-28 10:08:24 +0200

    Fixes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,3 @@
 <x-layouts.app :title="__('Dashboard')">
-    Choose a project on the right to get started
+    Choose a project on the menu to get started
 </x-layouts.app>
--- resources/views/livewire/tasks/item.blade.php
+++ resources/views/livewire/tasks/item.blade.php
@@ -5,8 +5,8 @@
         class="mb-4 flex items-center justify-between rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800">
         <div class="flex items-center">
             <input type="checkbox" wire:click={{ $task->is_completed ? 'handleUncheck' : 'handleCheck' }}
-                class="mr-4 h-5 w-5 rounded border-gray-300 text-blue-600 focus:ring-blue-500" {{ $task->is_completed ?
-    'checked' : '' }} wire:model="$task->is_completed" @disabled(Auth::user()->cannot('update', $task))>
+                class="mr-4 h-5 w-5 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
+                @checked($task->is_completed) @disabled(Auth::user()->cannot('update', $task))>
             <span class="{{ $task->is_completed ? 'line-through text-gray-500' : '' }}">{{ $task->text }}</span>
         </div>
 
